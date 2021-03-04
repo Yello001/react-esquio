df7fb557d23a39b156e426f6aaec7d4485b794f2
Yello <Yello001@users.noreply.github.com>
2021-03-04 12:39:08 +0100

    added impl

--- src/useFeatureFlags.ts
+++ src/useFeatureFlags.ts
@@ -1,3 +1,34 @@
-export function useFeatureFlags() {
-  return true;
+import { FeatureService, FeatureToggle, RestRequest } from 'esquio-js-connector';
+import React from 'react';
+
+export interface FeatureFlagConfig {
+  url: string;
+  apiKey: string;
+  product: string;
+  deployment?: string
+  toggles?: { [key: string]: FeatureToggle }
+}
+
+const FeatureFlagConfigContext = React.createContext<FeatureFlagConfig | undefined>(undefined);
+export const FeatureFlagProvider = FeatureFlagConfigContext.Provider;
+
+export function useFeatureFlags(...names: string[]): boolean[] {
+  const [result, setResult] = React.useState<boolean[]>(names.map(() => false));
+
+  const configContext = React.useContext(FeatureFlagConfigContext)
+  if (!configContext) {
+    throw Error('no FeatureFlagConfigContext found');
+  }
+
+  React.useEffect(() => {
+    const { toggles, ...requestConfig } = configContext;
+    const service = new FeatureService(new RestRequest(requestConfig), toggles);
+    Promise.all(names.map(name => service.isFeatureEnabled(name)))
+      .then(value => {
+        setResult(value);
+      })
+      .catch(reason => console.error(reason));
+  }, [setResult, configContext, ...names])
+
+  return result;
 }
--- test/useFeatureFlags.test.tsx
+++ test/useFeatureFlags.test.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
-import { useFeatureFlags } from '../src/useFeatureFlags';
+import { WrapperComponent } from '@testing-library/react-hooks/src/types/react';
+import { FeatureService, FeatureToggle, RestRequest } from 'esquio-js-connector';
+import React from 'react';
+import { FeatureFlagProvider, useFeatureFlags } from '../src';
+
+// noinspection JSUnusedLocalSymbols
+const isFeatureEnabledMock = jest.fn((name: string) => Promise.resolve(true));
+
+jest.mock('esquio-js-connector', () => ({
+  FeatureService: jest.fn().mockImplementation(() => ({
+    isFeatureEnabled: isFeatureEnabledMock
+  })),
+  RestRequest: jest.fn().mockImplementation(),
+}));
 
 describe('useFeatureFlags', () => {
-  it('should test', async () => {
-    const wrapper = (params: any) => (
-      <div>
-        {params.children}
-      </div>
-    );
-    const { result } = renderHook(() => useFeatureFlags(), { wrapper });
-    // await waitForNextUpdate();
+  it.each([true, false])('should return %s', async (expectedResult) => {
+      // noinspection JSUnusedLocalSymbols
+    isFeatureEnabledMock.mockImplementationOnce((name: string) => Promise.resolve(expectedResult));
 
-    const hasFeatureFlag = result.current;
-    expect(hasFeatureFlag).toBe(true);
-  });
+      const requestConfig = { apiKey: 'apiKey', url: 'http://test.org', product: 'product', deployment: 'deployment' };
+      const toggles: { [key: string]: FeatureToggle } = { 'testToggle': () => true };
+      const wrapper: WrapperComponent<React.PropsWithChildren<{}>> = ({ children }) => {
+        return (
+          <FeatureFlagProvider value={{ ...requestConfig, toggles }}>
+            {children}
+          </FeatureFlagProvider>
+        );
+      };
+
+      const { result, waitForNextUpdate } = renderHook(() => useFeatureFlags('testFlag'), { wrapper });
+      await waitForNextUpdate();
+
+      expect(result.current).toStrictEqual([expectedResult]);
+      expect(RestRequest).toBeCalledWith(requestConfig);
+      expect(FeatureService).toBeCalledWith({}, toggles);
+    }
+  );
 });
